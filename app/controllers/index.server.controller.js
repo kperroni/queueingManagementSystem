e98707b3edbcc1f1e4cad535294747894b3abc2e
kperroni <kenny.perroni@gmail.com>
2018-02-13 17:35:22 -0500

    Integrated MdBoostrap into the App - Added Nav and sample section

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,6 +1,6 @@
+const path = require('path');
+
 exports.render = function (req, res) {
-    const path = require('path');
     //Send all requests to Angular app
-     res.sendFile(path.join(__dirname, '../../' ,'dist/index.html'));
-     
+     res.sendFile(path.join(__dirname, '../../' ,'dist/index.html'));   
 };
