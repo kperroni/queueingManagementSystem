5ee8546efc417c8a6a58b1e9c06230f8887a6d87
Sergio Monroy Escalante <sergio.monroy.escalante@gmail.com>
2018-03-16 18:52:23 -0400

    draft of createTicket

---
+++ src/app/modules/service/service.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+@Injectable()
+export class ServiceService {
+
+  constructor(private http: HttpClient) { }
+
+  getServices()
+  {
+    return this.http.get('/getServices');
+  }  
+
+}
