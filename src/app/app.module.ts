e43b45651b466c82c5f33846a8db4e0c21b08540
LAPTOP-GPREV3N6\jajan <jajanpreetsingh15@gmail.com>
2018-03-29 14:09:10 -0400

    resolved app module conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,11 +21,8 @@ import { CreateTicketComponent } from './modules/ticket/components/create-ticket
 import { ViewActiveTicketsComponent } from './modules/ticket/components/view-active-tickets/view-active-tickets.component';
 import { AppSessionService } from './shared/services/session/session.service';
 import { MessageService } from './shared/services/messages/message.service';
-<<<<<<< HEAD
 import { ViewStudentTicketComponent } from './modules/ticket/components/view-student-ticket/view-student-ticket.component';
-=======
 import { StudentService } from './modules/student/student.service';
->>>>>>> refs/remotes/origin/master
 
 const appRoutes: Routes = [
 
