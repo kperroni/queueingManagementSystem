0b09206c66438aade4529913a1fde5d891b9a8c6
kperroni <kenny.perroni@gmail.com>
2018-04-13 16:39:29 -0400

    Fixed bugs on master. Guest component has been commented out

--- src/app/shared/services/messages/message.service.ts
+++ src/app/shared/services/messages/message.service.ts
@@ -4,28 +4,28 @@ import { Injectable } from '@angular/core';
 export class MessageService {
 
    message: any = {
-     "type":"",
-     "title":"",
-     "body":"",
-     "clear":"1"
+     'type': '',
+     'title': '',
+     'body': '',
+     'clear': '1'
    }
 
   constructor() { }
 
-setMessage(type, title, body){
+setMessage(type, title, body) {
   this.message.type = type;
   this.message.title = title;
   this.message.body = body;
-  this.message.clear = "0";
+  this.message.clear = '0';
 }
 
-getMessage(){
+getMessage() {
   return this.message;
 }
 
-clearMessage(){
-  this.message.type = "";
-  this.message.title = "";
-  this.message.body = "";
+clearMessage() {
+  this.message.type = '';
+  this.message.title = '';
+  this.message.body = '';
 }
 }
